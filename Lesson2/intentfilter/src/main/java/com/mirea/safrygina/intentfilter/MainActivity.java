74af38066915618306396a0366ba6bdba5c3ec8c
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-04-08 20:20:36 +0300

    process

--- Lesson2/intentfilter/src/main/java/com/mirea/safrygina/intentfilter/MainActivity.java
+++ Lesson2/intentfilter/src/main/java/com/mirea/safrygina/intentfilter/MainActivity.java
@@ -1,24 +1,40 @@
 package com.mirea.safrygina.intentfilter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button buttonOpenBrowser = findViewById(R.id.button_open_browser);
+        Button buttonShareInfo = findViewById(R.id.button_share_info);
+
+        buttonOpenBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri address = Uri.parse("https://www.mirea.ru/");
+                Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+                startActivity(openLinkIntent);
+            }
+        });
+
+        buttonShareInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "Сафрыгина Анастасия, РТУ МИРЭА");
+                startActivity(Intent.createChooser(shareIntent, "МОИ ФИО"));
+            }
         });
     }
 }
