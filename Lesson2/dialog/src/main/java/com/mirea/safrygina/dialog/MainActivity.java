5d86f1e092253fa48bb1ab10d2fc539c9710bc48
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-04-08 22:29:40 +0300

    done

--- Lesson2/dialog/src/main/java/com/mirea/safrygina/dialog/MainActivity.java
+++ Lesson2/dialog/src/main/java/com/mirea/safrygina/dialog/MainActivity.java
@@ -6,14 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClickShowDialog(View view) {
-        AlertDialogFragment dialogFragment = new AlertDialogFragment();
-        dialogFragment.show(getSupportFragmentManager(), "mirea");
+        findViewById(R.id.button_show_time_dialog).setOnClickListener(v -> {
+            MyTimeDialogFragment timeDialog = new MyTimeDialogFragment();
+            timeDialog.show(getSupportFragmentManager(), "timePicker");
+        });
+
+        findViewById(R.id.button_show_date_dialog).setOnClickListener(v -> {
+            MyDateDialogFragment dateDialog = new MyDateDialogFragment();
+            dateDialog.show(getSupportFragmentManager(), "datePicker");
+        });
+
+        findViewById(R.id.button_show_progress_dialog).setOnClickListener(v -> {
+            MyProgressDialogFragment progressDialog = new MyProgressDialogFragment();
+            progressDialog.show(getSupportFragmentManager(), "progress");
+        });
     }
 }
