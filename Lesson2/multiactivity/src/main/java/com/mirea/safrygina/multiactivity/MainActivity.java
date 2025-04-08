ae518b64c7fc5ce82f0b69729aef8aabd1c0cf3a
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-04-08 15:40:41 +0300

    process

--- Lesson2/multiactivity/src/main/java/com/mirea/safrygina/multiactivity/MainActivity.java
+++ Lesson2/multiactivity/src/main/java/com/mirea/safrygina/multiactivity/MainActivity.java
@@ -2,29 +2,33 @@ package com.mirea.safrygina.multiactivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        }
 
-    public void onClickNewActivity(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+
+        editText = findViewById(R.id.edit);
     }
+        public void onClickNewActivity(View view) {
 
-}
+            String textToSend = editText.getText().toString();
+
+            Intent intent = new Intent(this, SecondActivity.class);
+
+            intent.putExtra("input_text", textToSend);
+
+            startActivity(intent);
+        }
+    }
--- Lesson2/multiactivity/src/main/java/com/mirea/safrygina/multiactivity/SecondActivity.java
+++ Lesson2/multiactivity/src/main/java/com/mirea/safrygina/multiactivity/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.mirea.safrygina.multiactivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
@@ -9,5 +10,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        String inputText = getIntent().getStringExtra("input_text");
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(inputText);
     }
 }
