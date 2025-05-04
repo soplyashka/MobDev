66c7a7eeceb34e7b4d1ff6e235eaa3fefbfec2f1
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-05-04 11:56:24 +0300

    add pr4

--- Lesson4/Lesson4/app/src/main/java/com/mirea/safrygina/lesson4/MainActivity.java
+++ Lesson4/Lesson4/app/src/main/java/com/mirea/safrygina/lesson4/MainActivity.java
@@ -2,23 +2,69 @@ package com.mirea.safrygina.lesson4;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.mirea.safrygina.lesson4.databinding.ActivityMainBinding;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private MediaPlayer mediaPlayer;
+    private boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textViewTitle.setText("Алло-алло, я больше не доступна!");
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.allo);
+
+        binding.buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer != null) {
+                    if (isPlaying) {
+                        mediaPlayer.pause();
+                        binding.buttonPlay.setText("▶️");
+                        Log.d("Player", "Пауза");
+                    } else {
+                        mediaPlayer.start();
+                        binding.buttonPlay.setText("⏸");
+                        Log.d("Player", "Играет");
+                    }
+                    isPlaying = !isPlaying;
+                }
+            }
         });
+
+        binding.buttonPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Player", "Кнопка назад");
+            }
+        });
+
+        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Player", "Кнопка вперед");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
