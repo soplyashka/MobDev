809c269f45ef7b95ac192ab05d805728da39e247
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-05-07 00:54:52 +0300

    add mireaproject

---
+++ Lesson4/MireaProject/app/src/main/java/com/mirea/safrygina/mireaproject/DataFragment.java
@@ -0,0 +1,23 @@
+package com.mirea.safrygina.mireaproject;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.mirea.safrygina.mireaproject.R;
+
+public class DataFragment extends Fragment {
+
+    public DataFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_data, container, false);
+    }
+}
