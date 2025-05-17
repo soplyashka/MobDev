29bba996f7ce59b283a0021b99ffb1b53ef68fe1
Anastasiya Safrygina <nsafrigina@gmail.com>
2025-05-17 19:54:05 +0300

    add mireaproject

--- Lesson6/MireaProject/app/src/main/java/com/mirea/safrygina/mireaproject/MainActivity.java
+++ Lesson6/MireaProject/app/src/main/java/com/mirea/safrygina/mireaproject/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_data, R.id.nav_web, R.id.nav_microphone,
-                R.id.nav_altitude, R.id.nav_camera, R.id.nav_worker)
+                R.id.nav_altitude, R.id.nav_camera, R.id.nav_worker, R.id.nav_profile, R.id.nav_file)
 
                 .setOpenableLayout(drawer)
                 .build();
